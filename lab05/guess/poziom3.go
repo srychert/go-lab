6ba7c3b0b987a96c7d87ad1f4eaeb3c34c8e71c1
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-04-07 21:08:36 +0200

    poziom3

--- lab05/guess/poziom3.go
+++ lab05/guess/poziom3.go
@@ -8,87 +8,39 @@ import (
 	"time"
 )
 
-type input2 interface {
-	int | string
-}
-
-type Input struct {
-	guess   int
-	command string
-}
-
-func setInput(i interface{}) Input {
-	var input Input
-	switch v := i.(type) {
-	case nil:
-		fmt.Println("x is nil") // here v has type interface{}
-	case int:
-		fmt.Println("x is", v) // here v has type int
-		input.guess = i.(int)
-	case string:
-		fmt.Println("x is string") // here v has type interface{}
-		input.command = i.(string)
-	}
-	return input
-}
-
-func getUserInput() (int, string) {
-
-	var test any
-	var input interface{}
-	fmt.Scan(&input)
-	fmt.Println(input, test)
-	i := setInput(input)
-
-	return i.guess, i.command
-
-}
-
 func Poziom3() {
 	rand.Seed(time.Now().Unix())
 	upperBound := 100
 	computerNumber := rand.Intn(upperBound) + 1
-	var userGuess int
+	input := Input{0, ""}
 
-	var score int = 0
-
-	type Games struct {
-		names  []string
-		scores []int
-	}
-
-	var games Games
-
-	fmt.Println("Teraz będziesz zgadywać liczbę, którą wylosowałem")
+	fmt.Printf("Teraz będziesz zgadywać liczbę, którą wylosowałem z przedziału [1, %v]\n", upperBound)
 	fmt.Println("Napisz 'koniec' aby wyjść")
 
-	for userGuess != computerNumber {
-		score += 1
+	for input.guess != computerNumber {
 		fmt.Print("Podaj liczbę: ")
-		userGuess, command := getUserInput()
-
-		if command == "koniec" {
+		input = getUserInput()
+		if input.command == "koniec" {
+			fmt.Println("Żegnaj")
 			os.Exit(0)
 		}
-		if userGuess < computerNumber {
+		if input.guess < computerNumber && input.guess != 0 {
 			fmt.Println("Za mała")
 		}
-		if userGuess > computerNumber {
+		if input.guess > computerNumber && input.guess != 0 {
 			fmt.Println("Za duża")
 		}
 	}
-	fmt.Println("Brawo wygrałeś!")
-	var name string
-	fmt.Println("Jak masz na imie?")
-	fmt.Scan(&name)
-	games.names = append(games.names, name)
-	games.scores = append(games.scores, score)
 
+	fmt.Println("Brawo wygrałeś!")
 	var newGame string
+NEWGAME:
 	fmt.Print("Gramy jeszcze raz? [T/N]: ")
-	fmt.Scan(&newGame)
+	fmt.Scanln(&newGame)
 	if strings.ToUpper(newGame) == "T" {
 		Poziom3()
+	} else if strings.ToUpper(newGame) == "N" {
+		os.Exit(0)
 	}
-	fmt.Println(games)
+	goto NEWGAME
 }
